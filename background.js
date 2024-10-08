62620cd135f26d6621529038a2782cd52cfac09d
Karel Petruzela <karel.petruzela@merck.com>
2024-10-08 12:49:20 +0200

    Updated to have alt mapping

--- background.js
+++ background.js
@@ -10,14 +10,17 @@ browser.commands.onCommand.addListener((strName) => {
 		currentWindow: true,
 		hidden: false
 	}).then((arrTabs) => {
+
 		// Sort the tab array by position just in case it isn't already
 		console.log(arrTabs);
 		arrTabs.sort( function(a,b) {return (a.index - b.index);} )
 		console.log(arrTabs);
+
 		// Locate the active tab in the array
 		var nActive = arrTabs.findIndex( oTab => oTab.active === true );
+
 		// Make the appropriate tab active instead
-		if (strName === 'left-tab'){
+		if (strName === 'left-tab' && strName === "left-tab-alt"){
 			// Figure out the tab id
 			if (nActive > 0){
 				var newId = arrTabs[nActive - 1].id;
@@ -27,7 +30,7 @@ browser.commands.onCommand.addListener((strName) => {
 			}
 			// Make it active
 			browser.tabs.update(newId, {active:true});
-		} else if (strName === 'right-tab'){
+		} else if (strName === 'right-tab' && strName === "right-tab-alt"){
 			// Figure out the tab id
 			if (nActive < (arrTabs.length - 1)){
 				newId = arrTabs[nActive + 1].id;
